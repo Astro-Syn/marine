b4cf4be860b3e70637f691d17b35d8a8d4ee132b
Astro-Syn <kelseyamyb@gmail.com>
2026-05-05 11:32:34 -0400

    fix mobile

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 import './Navbar.css';
 
+
 export default function Navbar() {
   return (
     <div className="navbar-wrapper">
       <div className="navbar-container">
-
+       
         <NavLink
           to="/about"
           className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
